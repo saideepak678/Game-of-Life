00723890176672e65b4bffa0cdb4296cd6bbc76d
saideepak678 <deepaksai678@gmail.com>
2022-07-26 09:44:10 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,16 +3,6 @@ def buildJar() {
     sh 'mvn clean install'
 } 
 
-def BuildImageForDocker() {
-    echo "building the docker image..."
-     withCredentials([usernamePassword(credentialsId: 'Docker-login', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-              sh "docker rmi -f dsai83436/tomcatup"
-              sh "docker build -t dsai83436/tomcatup -f gameoflife-web/Dockerfile ."
-              sh "echo $PASS | docker login -u $USER --password-stdin"
-              sh "docker push dsai83436/tomcatup"
-         
-} 
-
 def deployApp() {
     
         sshPublisher(publishers: [sshPublisherDesc(configName: 'Docker', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''cd //opt/tomcat/tomcat/bin/

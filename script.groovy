ff79b6df096f8e94b0681b4a903d39df01e28808
saideepak678 <deepaksai678@gmail.com>
2022-07-26 09:25:51 -0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -14,7 +14,10 @@ def BuildImageForDocker() {
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+    
+        sshPublisher(publishers: [sshPublisherDesc(configName: 'Docker', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''cd //opt/tomcat/tomcat/bin/
+                ./catalina.sh start''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '//opt/tomcat/tomcat/webapps', remoteDirectorySDF: false, removePrefix: 'gameoflife-web/target', sourceFiles: 'gameoflife-web/target/gameoflife.war')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+   
 } 
 
 return this
